6e24a90c075dd275edb6819c0c964ee59dedd8a6
Young Jae <kyoje11@naver.com>
2016-07-06 02:07:22 +0900

    Update SoundCatcher.js
    SoundCatcher.js 함수 정리

--- Server/src/web/assets/js/SoundCatcher.js
+++ Server/src/web/assets/js/SoundCatcher.js
@@ -1,47 +1,68 @@
-//window.onload
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-var audioCtx = new AudioContext();
-
-var webSocketWorker = new Worker("assets/js/webSocket.js");
-
-var queue = [];
-
+var socket;
+var audioCtx;
+var queue = new Array();
 var startFlag = true;
+var receiveCnt = 0;
 
-webSocketWorker.onmessage = function(msg) {
+window.addEventListener('load', initAudio, false);
 
-	if (msg.data instanceof ArrayBuffer) {
-		console.log("MSG ARRIVE");
-		queue.push(msg.data);
-		console.log("QUEUE PUSH");
-		if (startFlag) {
-			console.log("START FLAG");
-			playingSound();
-			startFlag = false;
-		}
+function initAudio() {
+	try {
+		window.AudioContext = window.AudioContext || window.webkitAudioContext;
+		audioCtx = new AudioContext();
+		console.log("Web AudioContext Create");
+	} catch (e) {
+		alert('Web Audio API is not supported in this browser');
 	}
 }
-
-function playingSound() {
+function play() {
 	var audioData = queue.shift();
-	console.log(queue.length);
 	console.log("QUEUE GET DATA");
+	console.log("QUEUE Length : " + queue.length);
 	if (audioData instanceof ArrayBuffer) {
-		console.log("RECURSIVE FUNCTION");
 		audioCtx.decodeAudioData(audioData, function(audioBuffer) {
-			console.log("RECURSIVE DECODE DATA");
 			var src = audioCtx.createBufferSource();
 			src.buffer = audioBuffer;
 			src.connect(audioCtx.destination);
-			src.start(audioCtx.currentTime, 0, audioBuffer.length);
-			src.onended = playingSound();
+			src.start();
+			src.onended = play;
 		})
-	}else{
+	} else {
+		console.log("START FLAG ON");
 		startFlag = true;
 	}
 }
 
-function clientStart() {
-
+function webSocketConnection(){
+	var serverIP = "ws://" + prompt("Server IP 주소 입력해주세요") + ":80";
+	var project = document.getElementById('project');
+	var success = document.getElementById('success');
+	var msgCnt = document.getElementById('msgCnt');
+	
+	socket = new WebSocket(serverIP);
+	socket.binaryType = 'arraybuffer';
+	socket.onopen = function(event) {
+		console.log("Socket Open Success");
+		project.style.display = 'none';
+		success.style.display = 'block';
+	};
+	socket.onerror = function(event) {
+		alert("Socket Open Error Retry");
+		console.log("Socket Open Error");
+	};
+	socket.onmessage = function(msg) {
+		receiveCnt++;
+		console.log("MSG CNT : " + receiveCnt);
+		if (msg.data instanceof ArrayBuffer) {
+			console.log("MSG ARRIVE");
+			queue.push(msg.data);
+			console.log("QUEUE PUSH");
+			console.log("QUEUE Length : " + queue.length);
+			if (startFlag && queue.length > 3) {
+				console.log("START FLAG OFF");
+				startFlag = false;
+				play();				
+			}
+		}
+	};
 }
