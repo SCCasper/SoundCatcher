bfec63c46f306fab36406b7f956252ac15f2549d
nesoy <kyoje11@naver.com>
2016-09-21 21:09:59 +0900

    Web Server 추가

---
+++ Server/src/webCode/HttpMethod.java
@@ -0,0 +1,12 @@
+package web;
+
+public class HttpMethod {
+	public static final String METHOD_GET = "GET";
+    public static final String METHOD_HEAD = "HEAD";
+    public static final String METHOD_POST = "POST";
+    public static final String METHOD_PUT = "PUT";
+    public static final String METHOD_DELETE = "DELETE";
+    public static final String METHOD_CONNECT = "CONNECT";
+    public static final String METHOD_OPTIONS = "OPTIONS";
+    public static final String METHOD_TRACE = "TRACE";
+}
